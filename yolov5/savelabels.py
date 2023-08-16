96d004984a20e75d79047f0a3f51c681dc47f41e
Kaushal <kaushal.kirpekar@datakaveri.org>
2023-08-16 15:24:53 +0530

    Inference changes

--- yolov5/savelabels.py
+++ yolov5/savelabels.py
@@ -2,43 +2,33 @@
 import os 
 import json
 
-input_dir="runs/detect/exp/labels/"
-output_file="labels.json"
-input_dir2="./runOutput.txt"
+input_dir = "runs/detect/exp/labels/"
+output_file = "labels.json"
+input_dir2 = "./runOutput.txt"
 
 with open(input_dir2, "r") as runOutput_file:
-    outputContent= runOutput_file.read()
+    outputContent = runOutput_file.read()
 
-        
-with open(output_file, "a") as f:
-    f.write("{\"inference\":{\"inference\":{\"")
+output_data = {
+    "inference": {
+        "inference": {
+            "runOutput": outputContent,
+            "labels": []
+        }
+    }
+}
 
-with open(output_file) as f:
-    json_decoded = json.load(f)
-
-json_decoded["runOutput"] = outputContent
-
-with open(output_file, 'w') as f:
-    json.dump(json_decoded, f)
-
-with open(output_file, "a") as f:
-    f.write("\"},\"labels\":[")
-    
-first_file=True
 for file_name in os.listdir(input_dir):
     if file_name.endswith(".txt"):
-        file_path=os.path.join(input_dir,file_name)
-    if not first_file:
-        with open(output_file,"a") as f:
-            f.write(",")
+        file_path = os.path.join(input_dir, file_name)
+        
+        with open(file_path, "r") as input_file:
+            content = input_file.read()
+            
+        output_data["inference"]["inference"]["labels"].append({
+            "filename": file_name,
+            "content": content
+        })
 
-    with open(file_path, "r") as input_file, open(output_file,"a") as f:
-        content= input_file.read()
-        f.write(json.dumps({"filename": file_name, "content": content}))
-
-    first_file=False
-
-with open(output_file, "a") as f:
-    f.write("]}}}")
-
-
+with open(output_file, 'w') as f:
+    json.dump(output_data, f, indent=4)
