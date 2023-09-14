fa60de9a812050f4d1173423b7ba113bb9b199e5
Kaushal <kaushal.kirpekar@datakaveri.org>
2023-09-14 15:13:21 +0530

    profiling timestamp

--- profilingStep.sh
+++ profilingStep.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
 function profiling_func() {
-    # Read data from profiling.json if it exists
-    data=$(cat "profiling.json")
-
     local stepno="$1"
     local description="$2"
-
     local timestamp=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 
-    if [ -z "$(echo "$data" | jq '.stepsProfile')" ]; then
-        data=$(echo "$data" | jq '.stepsProfile = []')
+    if [ -f "profiling.json" ]; then
+        # Read data from profiling.json if it exists
+        data=$(jq -c '.' profiling.json)
+    else
+        # Initialize with default data if profiling.json doesn't exist
+        data='{
+            "input": {
+                "images": 0
+            },
+            "stepsProfile": [],
+            "totalTime": 0
+        }'
     fi
-    
-    # Construct the step object
-    local step_object="{\"step$stepno\": {\"description\": \"$description\", \"timestamp\": \"$timestamp\"}}"
-    
-    # Add the step object to 'stepsProfile' array
-    updated_data=$(echo "$data" | jq --argjson step "$step_object" '.stepsProfile += [$step]')
-    #write to file
-    echo "$updated_data" -> profiling.json
+
+    # Check if a step with the same number exists in the "stepsProfile" array
+    step_exists=$(echo "$data" | jq --arg stepno "$stepno" '.stepsProfile[] | has($stepno)')
+
+    if [ "$step_exists" = "true" ]; then
+        # Update the existing step with the same number
+        updated_data=$(echo "$data" | jq --arg stepno "$stepno" --arg description "$description" --arg timestamp "$timestamp" '.stepsProfile[] |= if has($stepno) then .[$stepno].description = $description | .[$stepno].timestamp = $timestamp else . end')
+    else
+        # Construct the step object
+        local step_object="{\"$stepno\": {\"description\": \"$description\", \"timestamp\": \"$timestamp\"}}"
+        
+        # Add the step object to 'stepsProfile' array
+        updated_data=$(echo "$data" | jq --argjson step "$step_object" '.stepsProfile += [$step]')
+    fi
+
+
+    # Write to file
+    echo "$updated_data" > profiling.json
 }
--- r.sh
+++ r.sh
@@ -1,5 +1,5 @@
 #call set state endpoint
-call_setstate_endpoint "Starting Application in SGX Enclave" 10 4 "Starting Application in SGX Enclave"
+call_setstate_endpoint "Starting Application in SGX Enclave" 10 5 "Starting Application in SGX Enclave"
 
 #calling profiling_func (step 5)
 profiling_func 5 "Starting Application in SGX Enclave" 
